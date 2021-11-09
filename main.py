612cbbca7e2d120457193d8a39ceb2546c58ad8c
nakashi94 <n0904yBUNJOU@gmail.com>
2021-11-09 22:36:25 +0900

    path parameters

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ import uvicorn
 app = FastAPI()
 
 
-@app.get("/")
-async def index():  # asyncは非同期処理を実行するために必要
-    return {"message": "Hello World", "user": "test"}
+@app.get("/countries/japan")
+async def japan():  # asyncは非同期処理を実行するために必要
+    return {"message": "This is Japan!"}
+
+
+@app.get("/countries/{country_name}")
+async def country(country_name: str):  # asyncは非同期処理を実行するために必要
+    return {"country_name": country_name}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=5000)
